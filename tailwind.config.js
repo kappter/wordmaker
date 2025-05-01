40fff9893dcfd15f36d522bf03de9279a7be042e
Ken Kapptie <kkapptie@graniteschools.org>
2025-05-01 14:12:42 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./*.js"], // Scan index.html and script.js for Tailwind classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
