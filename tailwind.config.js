a5c984d9504447a3afd28edc6d5b706ca55581cb
Ken Kapptie <kkapptie@graniteschools.org>
2025-05-02 11:05:47 -0600

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html", "./*.js"], // Scan index.html and script.js for Tailwind classes
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
